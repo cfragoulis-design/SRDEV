7ef3f94a5f2c6905ec274f8bff0f7e6f2c288272
cfragoulis-design <cfragoulis@gmail.com>
2026-03-16 21:20:27 +0200

    Delete Sklavounos-restaurants-main directory

--- Sklavounos-restaurants-main/app/utils.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-import re
-
-def slugify(name: str) -> str:
-    s = name.strip().lower()
-    s = re.sub(r"[^a-z0-9\s-]", "", s)
-    s = re.sub(r"\s+", "-", s)
-    s = re.sub(r"-+", "-", s)
-    return s[:150].strip("-") or "customer"
